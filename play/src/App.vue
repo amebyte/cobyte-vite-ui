06db8c8ff3431fa7c15fb329a2038bde25d7cd59
Cobyte <iamcobyte@gmail.com>
2025-11-08 22:44:17 +0800

    vite plugin

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-
+// import CoButton from 'cobyte-vite-ui/dist/components/button'
+// import 'cobyte-vite-ui/dist/style.css'
 </script>
 
 <template>
--- play/src/main.ts
+++ play/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import CobyteViteUI from 'cobyte-vite-ui'
-import 'cobyte-vite-ui/dist/style.css'
+// import CobyteViteUI from 'cobyte-vite-ui'
+// import 'cobyte-vite-ui/dist/style.css'
 
 const app = createApp(App)
-app.use(CobyteViteUI)
+// app.use(CobyteViteUI)
 app.mount('#app')
