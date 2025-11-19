cc2204d30bff913c1034a1dbfc9c3be60bf7e4fb
Cobyte <iamcobyte@gmail.com>
2025-11-19 20:40:43 +0800

    declare

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -3,3 +3,9 @@ declare module '*.vue' {
     const component: DefineComponent<{}, {}, any>
     export default component
 }
+
+declare module '@vitejs/plugin-vue' {
+    import { Plugin } from 'vite';
+    const vue: () => Plugin;
+    export default vue;
+}
